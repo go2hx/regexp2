acccfd7931788c654deae27930b5f21f04414f62
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-04 21:44:33 +0100

    README: write up and improve Example to be more Haxe styled

--- Example.hx
+++ Example.hx
@@ -1,10 +1,8 @@
 import github_dot_com.dlclark.regexp2.Regexp2;
 function main() {
-    final r = Regexp2.compile("^(abc|xyz)?(?(1)abc|xyz)", 0);
-    if (r.right != null) {
-        throw r.right;
-    }
-    trace(r.left.matchString("abc").left);
-    trace(r.left.matchString("xyz").left);
-    trace(r.left.matchString("def").left);
+    final r = Regexp2.mustCompile("^(abc|xyz)?(?(1)abc|xyz)", 0);
+    // left because it's a return tuple of (bool,error), right would give the error field
+    trace(r.matchString("abc").left);
+    trace(r.matchString("xyz").left);
+    trace(r.matchString("def").left);
 }
