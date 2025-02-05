37ecb0e94b25b526903804c4baf56b69e955bb1f
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-05 21:16:14 +0100

    Example: remove re2 in example

--- Example.hx
+++ Example.hx
@@ -1,6 +1,6 @@
 import github_dot_com.dlclark.regexp2.Regexp2;
 function main() {
-    final r = Regexp2.mustCompile("^(abc|xyz)?(?(1)abc|xyz)", Regexp2.rE2);
+    final r = Regexp2.mustCompile("^(abc|xyz)?(?(1)abc|xyz)");
     // left because it's a return tuple of (bool,error), right would give the error field
     trace(r.matchString("abc").left);
     trace(r.matchString("xyz").left);
