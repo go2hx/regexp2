e5097a9392722a16b60ff59d652df7d32e84b48b
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-05 21:18:53 +0100

    actions: run both example and test for each target

--- Make.hx
+++ Make.hx
@@ -2,38 +2,62 @@ function main() {
     final args = Sys.args();
     trace("run command: " + args[0]);
     switch args[0] {
-        /*case "test":
+		case "setup":
+			if (!sys.FileSystem.exists("regexp2"))
+				Sys.command("git clone --depth 1 https://github.com/dlclark/regexp2");
+			Sys.setCwd("regexp2");
+			if (sys.FileSystem.exists("golibs"))
+				Sys.command("rm -r -d golibs");
+			Sys.exit(Sys.command("haxelib run go2hx --rebuild --nodeps . --test --norun -compiler_nodejs --hxml test.hxml"));
+        case "test":
+			final excludes = "--exclude TestMarshal --exclude TestUnMarshal --exclude TestPcre_Basics --exclude TestIncorrectTimeoutError --exclude TestStopTimeoutClock";
+			Sys.setCwd("regexp2");
             final target = args[1];
             trace("target", target);
             final out = 'test.$target';
-            final buildCmd = buildTarget(target, out);
+            final buildCmd = buildTarget(target, out, "_internal.github_dot_com.dlclark.regexp2_dot_test.Regexp2_dot_test");
             Sys.command("haxe test.hxml " + buildCmd);
             final runCmd = runTarget(target, out, [], "");
-            if (runCmd != "")
-                Sys.command(runCmd);*/
+            if (runCmd != "") {
+                Sys.exit(Sys.command(runCmd));
+			}else{
 
-        case "example", "test": // TODO proper testing
-            Sys.command("haxe example.hxml");
+			}
+        case "example": // TODO proper testing
+		final target = args[1];
+		final out = 'test.$target';
+            var code = Sys.command("haxe example.hxml " + buildTarget(target,out, "Example"));
+			if (code != 0)
+				Sys.exit(code);
+			final runCmd = runTarget(target, out, [], "");
+            if (runCmd != "") {
+                Sys.exit(Sys.command(runCmd));
+			}else{
+				Sys.println(runCmd);
+			}
+		case "actions", "action":
+			Sys.exit(Sys.command("haxe scripts/github-actions/build.hxml"));
         case "build":
             Sys.command("rm -r -d golibs");
             // -nodeps
-            Sys.command("haxelib run go2hx https://github.com/dlclark/regexp2 -compiler_nodejs");
+			Sys.command("haxelib run go2hx extraParams");
+            Sys.exit(Sys.command("haxelib run go2hx --rebuild --nodeps https://github.com/dlclark/regexp2 -compiler_nodejs"));
         case "version":
             // TODO set version
         case "dev":
-            Sys.command("haxelib dev go2hx_regexp2 .");
+            Sys.exit(Sys.command("haxelib dev go2hx_regexp2 ."));
     }
 }
 
 
-function buildTarget(target:String, out:String):String {
+function buildTarget(target:String, out:String, main:String):String {
 	return switch target {
 		case "hl":
 			'--hl $out';
 		case "jvm", "cpp", "cs", "js", "lua", "python", "php", "neko":
 			'--$target $out';
 		case "interp":
-			"--interp";
+			'--run $main';
 		default:
 			throw "unknown target: " + target;
 	}
