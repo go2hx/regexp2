3773699c474aaa002c8e5771b2dcdd3cfae16091
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-05 23:49:23 +0100

    TypeInfo: revert to manual naming

--- golibs/stdgo/_internal/internal/TypeInfo.hx
+++ golibs/stdgo/_internal/internal/TypeInfo.hx
@@ -1,7 +1,7 @@
 package stdgo._internal.internal;
 class TypeInfo {
     public static var names:TypeInternalMap = {
-        final names:TypeInternalMap = std.Type.createInstance(std.Type.resolveClass('TypeInfoData_go2hx_'),[]).names;
+        final names:TypeInternalMap = new TypeInfoData_go2hx_().names;
         /*try {
             final hxb:TypeInternalMap = std.Type.createInstance(stdgo._internal.internal.HxbTypeInfoData,[]).names;
             for (key => value in hxb) {
