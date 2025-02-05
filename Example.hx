19aa67061c11ba55e137aaab207f7692ec723260
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-05 21:20:04 +0100

    Example: add arg option 0

--- Example.hx
+++ Example.hx
@@ -1,6 +1,6 @@
 import github_dot_com.dlclark.regexp2.Regexp2;
 function main() {
-    final r = Regexp2.mustCompile("^(abc|xyz)?(?(1)abc|xyz)");
+    final r = Regexp2.mustCompile("^(abc|xyz)?(?(1)abc|xyz)", 0);
     // left because it's a return tuple of (bool,error), right would give the error field
     trace(r.matchString("abc").left);
     trace(r.matchString("xyz").left);
